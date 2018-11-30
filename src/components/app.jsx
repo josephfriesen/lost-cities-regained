ba44692d851e7aed868ff3baf86419da6402653e
Joseph Friesen <friesen.josephc@gmail.com>
2018-11-30 11:30:20 -0800

    Initial commit; set up dev environment.

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Switch, Route, withRouter } from 'react-router-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <pre>Place application ====> here</pre>
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  
+}
+
+export default withRouter(App);
---
+++ src/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
+
+const render = (Component) => {
+  ReactDOM.render(
+      <HashRouter>
+        <Provider store={store}>
+          <Component/>
+        </Provider>
+      </HashRouter>,
+    document.getElementById('react-app-root')
+  );
+};
+
+render(App);
+
+/*eslint-disable */
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  })
+}
